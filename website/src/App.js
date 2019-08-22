a60c2dab85659444bfbd994cc1df187f23eb1e88
koladeore <koladeore@gmail.com>
2019-08-22 10:46:22 +0100

    setupreact-router&bootstrap

---
+++ website/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route} from 'react-router-dom';
+import './App.css';
+
+import HomePage from './Pages/Home/index.js';
+import AboutPage from './Pages/About/index.js';
+
+function App(){
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route path="/about" component={AboutPage}></Route>
+      </Switch>
+    </div>
+  );
+}
+export default App;
