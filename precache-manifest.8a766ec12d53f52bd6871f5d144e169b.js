180e2a437529db7850e4882ac3d350c4b0a81f6d
koladeore <koladeore@gmail.com>
2019-08-27 23:45:22 +0100

    Updates

---
+++ precache-manifest.8a766ec12d53f52bd6871f5d144e169b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a7770e0e9af7c79b1adac236bdb798c",
+    "url": "/koladeore/internship-website/tree/develop/website/src/index.html"
+  },
+  {
+    "revision": "00923d4392db82ce1b4c",
+    "url": "/koladeore/internship-website/tree/develop/website/src/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d2774424866fef613b26",
+    "url": "/koladeore/internship-website/tree/develop/website/src/static/css/main.61f4ef5f.chunk.css"
+  },
+  {
+    "revision": "00923d4392db82ce1b4c",
+    "url": "/koladeore/internship-website/tree/develop/website/src/static/js/2.c3797ff4.chunk.js"
+  },
+  {
+    "revision": "d2774424866fef613b26",
+    "url": "/koladeore/internship-website/tree/develop/website/src/static/js/main.9c827291.chunk.js"
+  },
+  {
+    "revision": "87fab1aa8058755d74dd",
+    "url": "/koladeore/internship-website/tree/develop/website/src/static/js/runtime~main.ca756d3b.js"
+  },
+  {
+    "revision": "189fae3070f60011d4afa5c6b6a0438d",
+    "url": "/koladeore/internship-website/tree/develop/website/src/static/media/about-image.189fae30.jpg"
+  },
+  {
+    "revision": "30a77886bca0f8b4abe05d72f247cd9a",
+    "url": "/koladeore/internship-website/tree/develop/website/src/static/media/bible_image.30a77886.jpg"
+  },
+  {
+    "revision": "dd64d7345811f1852fe20c4ce4c65f84",
+    "url": "/koladeore/internship-website/tree/develop/website/src/static/media/logo_church.dd64d734.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/koladeore/internship-website/tree/develop/website/src/precache-manifest.8a766ec12d53f52bd6871f5d144e169b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/koladeore/internship-website/tree/develop/website/src/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
